fd9c4fb1a65dc6a014479d21fa221498a8cdac7d
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-07-19 16:11:46 +0200

    initial commit

---
+++ snake.php
@@ -0,0 +1,17 @@
+<?php
+
+declare (strict_types = 1);
+
+readline_callback_handler_install('', function() { });
+
+$block = html_entity_decode('&#x2588;', ENT_NOQUOTES, 'UTF-8');
+
+for($i=0; $i<5; $i++) {
+    fwrite(STDOUT, PHP_EOL);
+}
+
+while (true) {
+    $char = fread(STDIN, 1);
+    echo $char;
+    usleep(5000);
+}
